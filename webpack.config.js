f5942d9be9713dc4dd774aef6db47c57d9a6fad4
james lockwood <ksjjl05@gmail.com>
2022-06-09 20:48:44 -0500

    push from last work

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,27 @@
- const path = require('path');
-
+const path = require('path');
  const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-
  module.exports = {
-  entry: './src/index.js',
-  entry: {
-    index: './src/index.js',
-    print: './src/print.js',
+   mode: 'development',
+   entry: {
+     index: './src/index.js',
+     print: './src/print.js',
+   },
+   devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Output Management',
-    }),
-  ],
+   plugins: [
+     new HtmlWebpackPlugin({
+       title: 'Development',
+     }),
+   ],
    output: {
-    filename: 'bundle.js',
-    filename: '[name].bundle.js',
+     filename: '[name].bundle.js',
      path: path.resolve(__dirname, 'dist'),
      clean: true,
    },
+  optimization: {
+    runtimeChunk: 'single',
+  },
  };
