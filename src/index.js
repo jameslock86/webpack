2d73a2e5377f1deff38aea9d68907f0e5acd61c6
james lockwood <ksjjl05@gmail.com>
2022-06-14 18:19:05 -0500

    adding code spliting

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
-import _ from 'lodash';
-import printMe from './print.js';
 
-function component() {
-    const element = document.createElement('div');
-    const btn = document.createElement('button');
-    
+  async function getComponent() {
+     const element = document.createElement('div');
+    const { default: _ } = await import('lodash');
+  
+   
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-    btn.innerHTML = 'click me and check that console';
-    btn.onclick = printMe;
-
-    
-    element.appendChild(btn);
-
     return element;
-  }
+   }
   
-  document.body.appendChild(component());
+   getComponent().then((component) => {
+     document.body.appendChild(component);
+   });
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,39 @@
-const path = require('path');
- const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const path = require('path');
+//  const HtmlWebpackPlugin = require('html-webpack-plugin');
 
- module.exports = {
-   mode: 'development',
-   entry: {
-     index: './src/index.js',
-     print: './src/print.js',
-   },
-   devtool: 'inline-source-map',
-   devServer: {
-     static: './dist',
-   },
-   plugins: [
-     new HtmlWebpackPlugin({
-       title: 'Development',
-     }),
-   ],
-   output: {
-     filename: '[name].bundle.js',
-     path: path.resolve(__dirname, 'dist'),
-     clean: true,
-    publicPath: '/',
-   },
- };
+//  module.exports = {
+//    mode: 'development',
+//    entry: {
+//      index: './src/index.js',
+//      print: './src/print.js',
+//    },
+//    devtool: 'inline-source-map',
+//    devServer: {
+//      static: './dist',
+//    },
+//    plugins: [
+//      new HtmlWebpackPlugin({
+//        title: 'Development',
+//      }),
+//    ],
+//    output: {
+//      filename: '[name].bundle.js',
+//      path: path.resolve(__dirname, 'dist'),
+//      clean: true,
+//     publicPath: '/',
+//    },
+//  };
+
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+ 
+};
